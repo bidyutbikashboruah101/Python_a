91c3ac8ca66259fd8e7880cf3317c38a3901ef73
bidyutbikashboruah101 <bidyutbikashboruah789@gmail.com>
2022-07-26 14:51:44 +0530

    pythn

---
+++ second(redaing a file).py
@@ -0,0 +1,9 @@
+# r = read only
+# w = write only
+# r+ = read and write
+# a = append something at the end of the file
+sn_file = open('second(file to read).txt', 'r')
+# print(sn_file.readable())    # to check if the file is readable
+# print(sn_file.readline())      # to read the first line of the file
+print(sn_file.read())          # tp read the whole file at once
+sn_file.close()
